9b577d32522bd9574343b80543c39e7dcc33384f
sitimper <sitimper@gmail.com>
2025-10-19 19:45:58 +0200

    feat: add dark mode

--- frontend/react/pages/Letters/Letters.jsx
+++ frontend/react/pages/Letters/Letters.jsx
@@ -1,23 +1,38 @@
-
 export default function Letters() {
-    return(
-        <main className="px-(--page-px) mt-4 flex flex-col items-center">
-            <div className="max-w-[1000px] w-full mb-8">
-                <h2 className="text-2xl font-bold">Hebrew alphabet</h2>
-                <img className="w-full" src="../../src/assets/letters/hebrewAlphabet.png" alt="Hebrew alphabet" />
-            </div>
-            <div className="max-w-[400px] w-full mb-8">
-                <h2 className="text-2xl font-bold">Hebrew final (sofit) letters</h2>
-                <img className="w-full" src="../../src/assets/letters/hebrewFinalLetters.png" alt="Hebrew final letters" />
-            </div>
-            <div className="max-w-[600px] w-full mb-8">
-                <h2 className="text-2xl font-bold">Hebrew niqqud</h2>
-                <img className="w-full" src="../../src/assets/letters/hebrewNiqqud.jpg" alt="Hebrew niqqud" />
-            </div>
-            <div className="max-w-[1000px] w-full mb-8">
-                <h2 className="text-2xl font-bold">Hebrew keyboard layout</h2>
-                <img className="w-full" src="../../src/assets/letters/hebrewKeyboardLayout.jpg" alt="Hebrew keyboard layout" />
-            </div>
-        </main>
-    );
+  return (
+    <main className="px-page-px pt-4 flex flex-col items-center bg-main text-text">
+      <div className="max-w-[1000px] w-full mb-8">
+        <h2 className="text-2xl font-bold">Hebrew alphabet</h2>
+        <img
+          className="w-full"
+          src="../../src/assets/letters/hebrewAlphabet.png"
+          alt="Hebrew alphabet"
+        />
+      </div>
+      <div className="max-w-[400px] w-full mb-8">
+        <h2 className="text-2xl font-bold">Hebrew final (sofit) letters</h2>
+        <img
+          className="w-full"
+          src="../../src/assets/letters/hebrewFinalLetters.png"
+          alt="Hebrew final letters"
+        />
+      </div>
+      <div className="max-w-[600px] w-full mb-8">
+        <h2 className="text-2xl font-bold">Hebrew niqqud</h2>
+        <img
+          className="w-full"
+          src="../../src/assets/letters/hebrewNiqqud.jpg"
+          alt="Hebrew niqqud"
+        />
+      </div>
+      <div className="max-w-[1000px] w-full mb-8">
+        <h2 className="text-2xl font-bold">Hebrew keyboard layout</h2>
+        <img
+          className="w-full"
+          src="../../src/assets/letters/hebrewKeyboardLayout.jpg"
+          alt="Hebrew keyboard layout"
+        />
+      </div>
+    </main>
+  );
 }
--- frontend/react/src/components/Navbar.jsx
+++ frontend/react/src/components/Navbar.jsx
@@ -1,18 +1,44 @@
+import { useTheme } from "../themeProvider";
 
 export default function Navbar() {
-    return (
-        <nav className="w-screen h-8 sticky top-0 left-0 flex justify-end items-center bg-(--primary)">
-            <ul className="h-full px-(--page-px) flex items-center text-white">
-            <li className="h-full flex items-center">
-                    <a className="h-full flex items-center px-4 bg-(--primary) hover:brightness-90" href="/links">Links</a>
-                </li>  
-                <li className="h-full flex items-center">
-                    <a className="h-full flex items-center px-4 bg-(--primary) hover:brightness-90" href="/letters">Letters</a>
-                </li>  
-                <li className="h-full">
-                    <a className="h-full flex items-center px-4 bg-(--primary) hover:brightness-90" href="/">Helper</a>
-                </li>  
-            </ul>
-        </nav>
-    );
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <nav className="w-screen h-8 sticky top-0 left-0 flex justify-end items-center bg-primary">
+      <ul className="h-full px-page-px flex items-center text-white">
+        <li className="h-full flex items-center">
+          <button
+            className="h-full flex items-center px-4 bg-primary hover:brightness-90"
+            onClick={toggleTheme}
+          >
+            {theme === "dark" ? <div>Dark</div> : <div>Light</div>}
+          </button>
+        </li>
+        <li className="h-full flex items-center">
+          <a
+            className="h-full flex items-center px-4 bg-primary hover:brightness-90"
+            href="/links"
+          >
+            Links
+          </a>
+        </li>
+        <li className="h-full flex items-center">
+          <a
+            className="h-full flex items-center px-4 bg-primary hover:brightness-90"
+            href="/letters"
+          >
+            Letters
+          </a>
+        </li>
+        <li className="h-full">
+          <a
+            className="h-full flex items-center px-4 bg-primary hover:brightness-90"
+            href="/"
+          >
+            Helper
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
